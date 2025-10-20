17dcee9861a3fbee15b8d95f0dfc3d379a9e198c
Rednax63 <113907511+Rednax63@users.noreply.github.com>
2025-10-20 10:08:14 +0100

    submitButton

--- game.py
+++ game.py
@@ -12,20 +12,28 @@ pygame.display.set_caption("The Game")
 manager = pygame_gui.UIManager((WIDTH,HEIGHT)) #creates ui manager
 
 #creates submit button
-button_rect = pygame.Ret((WIDTH - 130, HEIGHT - 70), (100,40))
+button_rect = pygame.Rect((WIDTH - 130, HEIGHT - 70), (100,40))
 button = pygame_gui.elements.UIButton(
     relative_rect = button_rect, 
     text = 'Submit',
     manager = manager
 )
 
-#game loop - keeps game updating until close
+clock = pygame.time.Clock()
+
+#game loop 
 running = True
 while running:
+    time_delta = clock.tick(60)/1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        manager.process_events(event)
+    
+    manager.update(time_delta) #updates gui state
+    window.fill((0, 0, 0))  # clears screen
+    manager.draw_ui(window) # draws gui elementw
     pygame.display.flip()   #update window
 
 #quit game
