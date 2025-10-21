61af1acda23cb74dd6a763d627e103d3fdef9ff4
Rednax63 <113907511+Rednax63@users.noreply.github.com>
2025-10-21 11:28:07 +0100

    empty window

---
+++ TitleScreen.py
@@ -0,0 +1,41 @@
+import pygame
+import pygame_gui
+import sys
+
+pygame.init()
+
+#setup window
+WIDTH, HEIGHT = 800, 600
+window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("The Game")
+
+manager = pygame_gui.UIManager((WIDTH,HEIGHT)) #creates ui manager
+
+
+clock = pygame.time.Clock()
+fullscreen = False
+running = True
+
+#game loop 
+while running:
+    time_delta = clock.tick(60)/1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        #handles window resizing
+        elif event.type == pygame.VIDEORESIZE: 
+            WIDTH, HEIGHT = event.w, event.h
+            window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+            manager.set_window_resolution((WIDTH, HEIGHT))
+
+        manager.process_events(event)
+    
+    manager.update(time_delta) #updates gui 
+    window.fill((99, 150, 47)) 
+    manager.draw_ui(window) # draws gui elements
+    pygame.display.flip()   #updates window
+
+#quit game
+pygame.quit()
+sys.exit()
