e297faa0595050e56d6aa024b49b3c5f4a0d34cf
Rednax63 <113907511+Rednax63@users.noreply.github.com>
2025-10-22 11:36:06 +0100

    restructured for window switching

--- TitleScreen.py
+++ TitleScreen.py
@@ -1,65 +1,57 @@
 import pygame
 import pygame_gui
-import sys
 
-pygame.init()
 
-#setup window
-WIDTH, HEIGHT = 800, 600
-window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-pygame.display.set_caption("Title Screen")
+class TitleScreen: 
+    def __init__(self, window, manager):
+        self.window = window
+        self.manager = manager #creates ui manager
 
-manager = pygame_gui.UIManager((WIDTH,HEIGHT)) #creates ui manager
+        self.WIDTH, self.HEIGHT = window.get_size()
 
-#adds title test
-font = pygame.font.SysFont("Arial", 96, bold = True)
-title = font.render("Untitled", True, (0,0,0))
-title_rect = title.get_rect(center=(WIDTH // 2, HEIGHT // 3 ))
+        #adds title test
+        self.font = pygame.font.SysFont("Arial", 96, bold = True)
+        self.title = self.font.render("Untitled", True, (0,0,0))
+        self.title_rect = self.title.get_rect(center=(self.WIDTH // 2, self.HEIGHT // 3 ))
 
-#creates log in button
-button_rect = pygame.Rect(100,150,150,45)
-button = pygame_gui.elements.UIButton(
-    relative_rect = button_rect, 
-    text = 'Log In',
-    manager = manager,
-    anchors={'centerx': 'centerx', 'centery': 'centery'}
-)
+        #creates log in button
+        loginbutton_rect = pygame.Rect(100,150,150,45)
+        self.loginbutton = pygame_gui.elements.UIButton(
+            relative_rect = loginbutton_rect, 
+            text = 'Log In',
+            manager = self.manager,
+            anchors={'centerx': 'centerx', 'centery': 'centery'}
+        )
 
-#creates sign up button
-button_rect = pygame.Rect(-100,150,150,45)
-button = pygame_gui.elements.UIButton(
-    relative_rect = button_rect, 
-    text = 'Sign Up',
-    manager = manager,
-    anchors={'centerx': 'centerx', 'centery': 'centery'}
-)
+        #creates sign up button
+        signupbutton_rect = pygame.Rect(-100,150,150,45)
+        self.signupbutton = pygame_gui.elements.UIButton(
+            relative_rect = signupbutton_rect, 
+            text = 'Sign Up',
+            manager = self.manager,
+            anchors={'centerx': 'centerx', 'centery': 'centery'}
+        )
 
-clock = pygame.time.Clock()
-fullscreen = False
-running = True
-
-#game loop 
-while running:
-    time_delta = clock.tick(60)/1000
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
+    def handle_event(self, event): #handles input and next screen
         #handles window resizing
-        elif event.type == pygame.VIDEORESIZE: 
-            WIDTH, HEIGHT = event.w, event.h
-            title_rect = title.get_rect(center=(WIDTH // 2, HEIGHT // 3))
-            window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-            manager.set_window_resolution((WIDTH, HEIGHT))
+        if event.type == pygame.VIDEORESIZE: 
+            self.WIDTH, self.HEIGHT = event.w, event.h
+            self.title_rect = self.title.get_rect(center=(self.WIDTH // 2, self.HEIGHT // 3))
+            self.window = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.RESIZABLE)
+            self.manager.set_window_resolution((self.WIDTH, self.HEIGHT))
 
-        manager.process_events(event)
-    
-    manager.update(time_delta) #updates gui 
-    window.fill((99, 150, 47)) 
-    window.blit(title, title_rect) #displays title
-    manager.draw_ui(window) # draws gui elements
-    pygame.display.flip()   #updates window
+        #manages button clicks
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == self.loginbutton: 
+                return "login"      #tells main script to switch to login menu
+            elif event.ui_element == self.signupbutton: 
+                return "signup"     #tells main script to switch to signup menu 
+        return None
 
-#quit game
-pygame.quit()
-sys.exit()
+    def update(self, time_delta):  
+        self.manager.update(time_delta) #updates gui 
+
+    def draw(self, window): 
+        window.fill((99, 150, 47)) 
+        window.blit(self.title, self.title_rect) #displays title
+        self.manager.draw_ui(window) # draws gui elements
