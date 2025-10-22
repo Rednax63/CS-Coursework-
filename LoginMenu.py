66a4c0907c48c95c34903abdd977e43944c6cfbc
Rednax63 <113907511+Rednax63@users.noreply.github.com>
2025-10-22 11:35:18 +0100

    re structured for window switching

--- LoginMenu.py
+++ LoginMenu.py
@@ -1,73 +1,62 @@
 import pygame
 import pygame_gui
-import sys
 
-pygame.init()
+class LoginScreen: 
+    def __init__(self, window, manager):
+        self.window = window 
+        self.manager = manager  #creates ui manager
+        self.WIDTH, self.HEIGHT = window.get_size()
+        
+        #creates submit button
+        submitbutton_rect = pygame.Rect(0,40,150,45)
+        self.submitbutton = pygame_gui.elements.UIButton(
+            relative_rect = submitbutton_rect, 
+            text = 'Submit',
+            manager = self.manager,
+            anchors={'centerx': 'centerx', 'centery': 'centery'}
+        )
+        #username input box
+        userinput_rect = pygame.Rect(0,-70,300,40)
+        self.userinput = pygame_gui.elements.UITextEntryLine(
+            relative_rect = userinput_rect,
+            manager = self.manager,
+            anchors={'centerx': 'centerx', 'centery' : 'centery'}
+        )
+        self.userinput.set_text("Username")
+        #password input box
+        passwordinput_rect = pygame.Rect(0,-20,300,40)
+        self.passwordinput = pygame_gui.elements.UITextEntryLine(
+            relative_rect = passwordinput_rect,
+            manager = self.manager,
+            anchors={'centerx': 'centerx', 'centery' : 'centery'}
+        )
+        self.passwordinput.set_text_hidden(True) #hides the password
+        self.passwordinput.set_text("Password")
 
-#setup window
-WIDTH, HEIGHT = 800, 600
-window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-pygame.display.set_caption("The Game")
-
-manager = pygame_gui.UIManager((WIDTH,HEIGHT)) #creates ui manager
-
-#creates submit button
-button_rect = pygame.Rect(0,40,150,45)
-button = pygame_gui.elements.UIButton(
-    relative_rect = button_rect, 
-    text = 'Submit',
-    manager = manager,
-    anchors={'centerx': 'centerx', 'centery': 'centery'}
-)
-#username input box
-userinput_rect = pygame.Rect(0,-70,300,40)
-userinput = pygame_gui.elements.UITextEntryLine(
-    relative_rect = userinput_rect,
-    manager = manager,
-    anchors={'centerx': 'centerx', 'centery' : 'centery'}
-)
-userinput.set_text("Username")
-#password input box
-passwordinput_rect = pygame.Rect(0,-20,300,40)
-passwordinput = pygame_gui.elements.UITextEntryLine(
-    relative_rect = passwordinput_rect,
-    manager = manager,
-    anchors={'centerx': 'centerx', 'centery' : 'centery'}
-)
-passwordinput.set_text_hidden(True) #hides the password
-passwordinput.set_text("Password")
-
-clock = pygame.time.Clock()
-fullscreen = False
-running = True
-
-#game loop 
-while running:
-    time_delta = clock.tick(60)/1000
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
+    def handle_event(self, event): #handles inputs, events 
+        self.manager.process_events(event)
         #handles window resizing
-        elif event.type == pygame.VIDEORESIZE: 
-            WIDTH, HEIGHT = event.w, event.h
-            window = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-            manager.set_window_resolution((WIDTH, HEIGHT))
+        if event.type == pygame.VIDEORESIZE: 
+            self.WIDTH, self.HEIGHT = event.w, event.h
+            self.window = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.RESIZABLE)
+            self.manager.set_window_resolution((self.WIDTH, self.HEIGHT))
 
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == button: 
-                username = userinput.get_text()
-                password = passwordinput.get_text()
+            if event.ui_element == self.submitbutton: 
+                username = self.userinput.get_text()
+                password = self.passwordinput.get_text()
                 print(f"Username: {username}")
                 print(f"Password: {password}")
+                #opens window if username and password correct
+                #return "title"
 
-        manager.process_events(event)
+        return None 
     
-    manager.update(time_delta) #updates gui 
-    window.fill((99, 150, 47)) 
-    manager.draw_ui(window) # draws gui elementw
-    pygame.display.flip()   #updates window
+    def update(self, time_delta):
+        self.manager.update(time_delta) #updates gui
+    
+    def draw(self, window):
+        window.fill((99, 150, 47)) 
+        self.manager.draw_ui(window) # draws gui elements
 
-#quit game
-pygame.quit()
-sys.exit()
+
